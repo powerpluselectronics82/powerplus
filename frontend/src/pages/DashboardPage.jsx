16f876a81ddc62549d380ab1fbb2e2fb02736831
Aman Kumar Singh <singhaman2321@gmail.com>
2026-09-15 08:01:44 +0530

    sixth2

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -4,7 +4,7 @@ import { useBranch } from '../context/BranchContext';
 import { productService } from '../services/productService';
 import { saleService } from '../services/saleService';
 import { TaxInvoiceModal } from '../components/pos/TaxInvoiceModal';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import {
   TrendingUp,
   AlertTriangle,
